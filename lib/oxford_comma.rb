4083a31d1ff8ad8c2fd6e92d047ac187ee26b4cd
nmhummel <natalie.hummel@gmail.com>
2020-10-22 12:27:07 -0500

    commit tree c27699198c8aec0c294e0a6561e790cc3b8060a4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+
+# returns a string without any additional formatting when given a 1-element array
+# array = ["kiwi"]        
+def oxford_comma(array)  # array/kiwi is the argument
+     
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array[-1] = "and " + array[-1]
+        array.join(", ")
+    else 
+        array.join
+    end
 
 end
+
+
+
+
+
+
+
+
